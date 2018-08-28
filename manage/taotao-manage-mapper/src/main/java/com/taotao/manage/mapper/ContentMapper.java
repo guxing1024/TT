8bd6e0bd3dfeb73353dd1527acee8731c5286bdd
guxing1024 <duzidengdai1024@163.com>
2018-08-28 20:22:10 +0800

    添加类目信息

--- manage/taotao-manage-mapper/src/main/java/com/taotao/manage/mapper/ContentMapper.java
+++ manage/taotao-manage-mapper/src/main/java/com/taotao/manage/mapper/ContentMapper.java
@@ -4,5 +4,9 @@ import com.github.abel533.mapper.Mapper;
 import com.taotao.manage.pojo.Content;
 import com.taotao.manage.pojo.ContentCategory;
 
+import java.util.List;
+
 public interface ContentMapper extends Mapper<Content> {
+
+    List<Content> queryListByContentCategoryId(Long categoryId);
 }
--- manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ContentService.java
+++ manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ContentService.java
@@ -1,8 +1,14 @@
 package com.taotao.manage.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.bean.EasyUIResult;
+import com.taotao.manage.mapper.ContentMapper;
 import com.taotao.manage.pojo.Content;
-import com.taotao.manage.pojo.ContentCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author
@@ -11,4 +17,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ContentService extends BaseService<Content> {
+    @Autowired
+    private ContentMapper contentMapper;
+
+    public EasyUIResult queryListByContentCategoryId(Long categoryId, Integer page, Integer rows) {
+        PageHelper.startPage(page, rows);
+        List<Content> list = contentMapper.queryListByContentCategoryId(categoryId);
+        PageInfo<Content> pageInfo = new PageInfo<>(list);
+        EasyUIResult easyUIResult = new EasyUIResult(pageInfo.getTotal(),pageInfo.getList());
+        return easyUIResult;
+    }
 }
